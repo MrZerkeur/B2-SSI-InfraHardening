65c4bda4d4ef031a7e529f413ad13be028ae1ac1
ahugo <hugo.andriamampianina@ynov.com>
2024-05-02 09:21:07 +0200

    fix white text on white bg for input

--- website/src/components/Forms/ContactForm.tsx
+++ website/src/components/Forms/ContactForm.tsx
@@ -9,28 +9,28 @@ export default function ContactForm() {
 
   return (
     <form action={formAction} className="flex flex-col justify-center items-center gap-2 w-1/2 h-[20rem] bg-gray-900 rounded-md">
-      <div className="flex flex-row gap-4 text-white">
-        <label htmlFor="firstName">Prénom *</label>
+      <div className="flex flex-row gap-4">
+        <label htmlFor="firstName" className="text-white">Prénom *</label>
         <input type="text" name="firstName" id="firstName" placeholder="Prénom..." required/>
       </div>
-      <div className="flex flex-row gap-4 text-white">
-        <label htmlFor="lastName">Nom de famille *</label>
+      <div className="flex flex-row gap-4">
+        <label htmlFor="lastName" className="text-white">Nom de famille *</label>
         <input type="text" name="lastName" id="lastName" placeholder="Nom..." required/>
       </div>
-      <div className="flex flex-row gap-4 text-white">
-        <label htmlFor="email">Adresse mail *</label>
+      <div className="flex flex-row gap-4">
+        <label htmlFor="email" className="text-white">Adresse mail *</label>
         <input type="email" name="email" id="email" placeholder="Adresse mail..." required/>
       </div>
-      <div className="flex flex-row gap-4 text-white">
-        <label htmlFor="tel">Numéro de téléphone</label>
+      <div className="flex flex-row gap-4">
+        <label htmlFor="tel" className="text-white">Numéro de téléphone</label>
         <input type="tel" name="tel" id="tel" />
       </div>
-      <div className="flex flex-row gap-4 text-white">
-      <label htmlFor="message">Message *</label>
+      <div className="flex flex-row gap-4">
+      <label htmlFor="message" className="text-white">Message *</label>
         <input type="text" name="message" id="message" placeholder="Ecrire message" required/>
       </div>
-      <div className="flex flex-row gap-4 text-white">
-        <label htmlFor="file">Image (optionnelle)</label>
+      <div className="flex flex-row gap-4">
+        <label htmlFor="file" className="text-white">Image (optionnelle)</label>
         <input type="file" name="file" id="file" accept="image/*"/>
       </div>
       <button className="text-white">Envoyer</button>
